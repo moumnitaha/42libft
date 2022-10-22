6849f09c774c6582fef9f1b262026a940463eb7d
tmoumni <tmoumni@student.42.fr>
2022-10-22 18:59:56 +0100

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 11:23:06 by tmoumni           #+#    #+#             */
-/*   Updated: 2022/10/22 18:33:22 by tmoumni          ###   ########.fr       */
+/*   Updated: 2022/10/22 18:59:37 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	main(int c, char **av)
 
 	if (c > 3)
 		return (0);
-	haystack = "TahaMoumniaThe geek at 1337";
+	haystack = "Taha Moumni at 1337";
 	i = 0;
 	array = ft_split(haystack, av[1][0]);
 	while (ft_split(haystack, av[1][0])[i])
