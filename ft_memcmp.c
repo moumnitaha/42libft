ac7cdc894a26e5f6e1ae230e441279beda1dd537
tmoumni <tmoumni@student.42.fr>
2022-10-23 15:18:00 +0100

    git

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,23 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/10 14:24:26 by tmoumni           #+#    #+#             */
-/*   Updated: 2022/10/23 14:25:20 by tmoumni          ###   ########.fr       */
+/*   Created: 2022/10/23 14:48:20 by tmoumni           #+#    #+#             */
+/*   Updated: 2022/10/23 15:17:30 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int c)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char	n;
+	size_t	count;
+	char	*str1;
+	char	*str2;
 
-	n = c;
-	if (n >= 65 && n <= 90)
-		n = n + 32;
-	return (n);
+	if (!s1 || !s2)
+		return (0);
+	str1 = (char *)s1;
+	str2 = (char *)s2;
+	count = 0;
+	while (count < n)
+	{
+		if ((unsigned char)*str1 != (unsigned char)*str2)
+			return ((unsigned char)*str1 - (unsigned char)*str2);
+		count++;
+		str1++;
+		str2++;
+	}
+	return (0);
 }
