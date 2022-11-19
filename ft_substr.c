54cd83d466816d33e923eb52c2c6ae570a607154
tmoumni <tmoumni@student.42.fr>
2022-11-19 19:28:09 +0100

    tmoumni

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,27 @@
 /*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 19:32:02 by tmoumni           #+#    #+#             */
-/*   Updated: 2022/11/19 15:03:06 by tmoumni          ###   ########.fr       */
+/*   Updated: 2022/11/19 19:24:07 by tmoumni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*stralloc(size_t start, size_t strlen, size_t len)
+{
+	char	*str;
+
+	if (start <= strlen)
+	{
+		if (len <= strlen - start)
+			str = (char *)malloc((len + 1) * sizeof(char));
+		else
+			str = (char *)malloc((strlen - start + 1) * sizeof(char));
+	}
+	else
+		str = (char *)malloc(sizeof(char));
+	return (str);
+}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -22,9 +38,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	count = 0;
 	strlen = ft_strlen(s);
-	if (len >= strlen)
+	if (len > strlen)
 		len = strlen;
-	str = (char *)malloc((len + 1) * sizeof(char));
+	str = stralloc(start, strlen, len);
 	if (!str)
 		return (NULL);
 	if (start < strlen)
